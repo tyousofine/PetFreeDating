231f763fd5ff86c4649df1d212e9b0b5c2cc4bfc
tyousofine <tyousofine@gmail.com>
2025-03-13 08:18:00 -0400

    initial set up

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+    const authToken = true;
+    const handleClick = () => {
+        console.log("clicked!")
+    }
+    return (
+        <div className='home'>
+            <h1>Swipe Right™</h1>
+            <button
+                className='primary-button'
+                onClick={handleClick}>
+
+                {authToken ? 'Signout' : 'Create Account'}
+            </button>
+
+
+        </div>
+    )
+}
+
+export default Home
